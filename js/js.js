3f71ef46b289245144c0138b481bbe8a2e7b61be
kirillandreev <reeper98@mail.ru>
2023-02-23 15:40:55 +0300

    add sliders, fix headers, add footer-levels

---
+++ js/js.js
@@ -0,0 +1,14 @@
+$('.slider3').slick({
+  infinite: true,
+  arrows: true
+});
+
+$('.slider2').slick({
+  infinite: true,
+  arrows: true
+});
+
+$('.slider1').slick({
+    infinite: true,
+    arrows: true
+});
